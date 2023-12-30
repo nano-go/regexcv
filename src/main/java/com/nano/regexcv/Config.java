e88bfc0e4797ec6f3bdd9351c115fd197b8bd449
nano-go <1522862112@qq.com>
2023-12-31 01:57:21 +0800

    Release v1.0.2

--- src/main/java/com/nano/regexcv/Config.java
+++ src/main/java/com/nano/regexcv/Config.java
@@ -16,6 +16,6 @@
 package com.nano.regexcv;
 
 public class Config {
-  public static final String VERSION = "v1.0.1";
-  public static final int VERSION_CODE = 2;
+  public static final String VERSION = "v1.0.2";
+  public static final int VERSION_CODE = 3;
 }
